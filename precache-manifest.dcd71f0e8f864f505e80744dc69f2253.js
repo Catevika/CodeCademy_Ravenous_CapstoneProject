a42b4567bd0d52ecf8049fa83db586cdee366523
Catevika <dominique.bello@outlook.fr>
2021-09-09 19:28:48 +0200

    Updates

---
+++ precache-manifest.dcd71f0e8f864f505e80744dc69f2253.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeed5cb8b411b6bc945852f5ec3ed740",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/index.html"
+  },
+  {
+    "revision": "46f5d6d9462f81921520",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/css/main.36b9acc5.chunk.css"
+  },
+  {
+    "revision": "aba63a5b41bfa59b8791",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/js/2.07416545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/js/2.07416545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f5d6d9462f81921520",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/js/main.ad290617.chunk.js"
+  },
+  {
+    "revision": "9eb423fb7a8f518ea871",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/js/runtime-main.0e14ddaa.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/CodeCademy_Ravenous_CapstoneProject/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
