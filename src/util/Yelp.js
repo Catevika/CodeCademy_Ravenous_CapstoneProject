32fc177b630e08ed75a68d8363758ddd995bc938
Catevika <dominique.bello@outlook.fr>
2021-09-23 09:49:32 +0200

    Hide API key in ENV variable

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,33 +1,38 @@
-const apiKey = 'h5-XcWPL7cMzYBpNdb3H50Nrp4tuqGfDKv1rfIro9a70YPEtuuBUbNHuDYxJLzOT0QRfFCUFUwA8vGaZLQguhkoSawt7LbUyOaWmZVymurtY06ar0_j6rIdSHGGPXHYx';
+const apiKey = process.env.REACT_APP_KEY;
 
 const Yelp = {
-    search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: {
-               Authorization: `Bearer ${apiKey}`
-            }
-            }).then((response) => {
-                return response.json();
-            }).then((jsonResponse) => {
-            if (jsonResponse.businesses) {
-                return jsonResponse.businesses.map((business) => {
-                    console.log(business);
-                    return {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        adress: business.location.adress1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories[0].title,
-                        rating: business.rating,
-                        reviewCount: business.review_count
-                    };
-                });
-            }
-        });
-    }
+	search(term, location, sortBy) {
+		return fetch(
+			`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`
+				}
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((jsonResponse) => {
+				if (jsonResponse.businesses) {
+					return jsonResponse.businesses.map((business) => {
+						console.log(business);
+						return {
+							id: business.id,
+							imageSrc: business.image_url,
+							name: business.name,
+							adress: business.location.adress1,
+							city: business.location.city,
+							state: business.location.state,
+							zipCode: business.location.zip_code,
+							category: business.categories[0].title,
+							rating: business.rating,
+							reviewCount: business.review_count
+						};
+					});
+				}
+			});
+	}
 };
 
 export default Yelp;
